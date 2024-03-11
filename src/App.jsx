55449684359402938d8f3a1bcbbbcf393741c3e0
chlangus <angusangus11@gmail.com>
2024-03-11 14:37:18 +0900

    Docs: tailwind 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div>
-      <h1>Hello, World!</h1>
+      <h1 className="text-red-50">Hello, World!</h1>
       <p>This is a simple React app.</p>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+export const content = [
+  './src/**/*.{js,ts,jsx,tsx}',
+  './pages/**/*.{js,ts,jsx,tsx}',
+  './components/**/*.{js,ts,jsx,tsx}'
+];
+export const theme = {
+  extend: {}
+};
+export const plugins = [];
