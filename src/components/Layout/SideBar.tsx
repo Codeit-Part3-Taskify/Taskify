5261b424b340df975d7e64479cfd5eb458d4d594
yulmai999 <37208116+yulmai999@users.noreply.github.com>
2024-03-15 19:39:28 +0900

    Design: 버튼,인풋 css추가
    -global.css에 @layer 를 이용해 재사용css세팅
    -사이드바 태그에 flex추가

--- src/components/Layout/SideBar.tsx
+++ src/components/Layout/SideBar.tsx
@@ -5,7 +5,7 @@ import LogoText from '../Sidebar/LogoText';
 
 export default function Sidebar() {
   return (
-    <div>
+    <div className="flex">
       <aside className="w-[6.7rem] h-lvh border-r-[0.1rem] border-gray_D9D9D9 flex-col pt-[2rem] tablet:w-[16rem] desktop:w-[30rem] desktop:px-[2.4rem]">
         <LogoText />
         <AddDashboardHeader />
--- src/components/MyPage/ProfileInputBox.tsx
+++ src/components/MyPage/ProfileInputBox.tsx
@@ -2,7 +2,17 @@ export default function ProfileBox() {
   return (
     <div>
       <div>프로필</div>
-      <div>프로필 박스</div>
+      <form>
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+        <button
+          type="button"
+          className=" bg-violet text-white w-[8.4rem] h-[3.2rem] rounded-[0.4rem]"
+        >
+          저장
+        </button>
+      </form>
     </div>
   );
 }
