074f454711873ee2c2af48add6bf947ebe12750e
Jnnsu <jspp120@gmail.com>
2024-03-16 18:52:58 +0900

    Fix: active 스타일 수정

--- src/components/Buttons/Button.tsx
+++ src/components/Buttons/Button.tsx
@@ -28,19 +28,19 @@ export default function Button({
   appendix
 }: ButtonProps) {
   const baseStyle = `flex justify-center items-center`;
-  const activeStyle = isActive
-    ? 'opacity-100'
-    : 'text-white bg-gray_9FA6B2 cursor-not-allowed';
   const variantStyles = {
     primary: 'bg-violet text-white',
     secondary: 'bg-white border border-gray_D9D9D9',
     ghost: 'bg-invisible border border-gray_D9D9D9'
   };
+  const activeStyle = isActive
+    ? `${variantStyles[variant]}`
+    : 'text-white bg-gray_9FA6B2 cursor-not-allowed';
 
   return (
     <button
       type={type}
-      className={`${baseStyle} ${activeStyle} ${variantStyles[variant]} ${customStyles}`}
+      className={`${baseStyle} ${activeStyle} ${customStyles}`}
       disabled={disabled}
       onClick={onClick}
       onBlur={onBlur}
