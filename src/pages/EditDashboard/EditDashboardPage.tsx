c388cfe8ac6d2fdf35a5dc69a56662a0b1a84fdc
Jnnsu <jspp120@gmail.com>
2024-03-16 19:20:46 +0900

    Style: 대시보드 삭제버튼 수정

--- src/pages/EditDashboard/EditDashboardPage.tsx
+++ src/pages/EditDashboard/EditDashboardPage.tsx
@@ -8,18 +8,18 @@ export default function EditDashboardPage() {
   return (
     <main className="mt-[2rem] ml-[2rem]">
       <ReturnButton />
-      <article className="flex flex-col gap-[1.2rem] mt-[2.5rem] mb-[4rem]">
+      <article className="flex flex-col gap-[1.2rem] mt-[2.5rem] mb-[5.6rem]">
         <EditDashboardTitle />
         <EditDashboardMembers />
         <InvitationSettings />
+        <Button
+          variant="ghost"
+          type="button"
+          customStyles="w-[32rem] h-[6.2rem] mt-[2.8rem] text-[1.8rem] rounded-[0.8rem] font-medium"
+        >
+          대시보드 삭제하기
+        </Button>
       </article>
-      <Button
-        variant="ghost"
-        type="button"
-        customStyles="px-[9.5rem] py-[2rem] text-[1.8rem] rounded-[0.8rem] mb-[5.6rem]"
-      >
-        대시보드 삭제하기
-      </Button>
     </main>
   );
 }
