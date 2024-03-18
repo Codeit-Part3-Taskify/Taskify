9fe060064a407be1b98c001168e3626c4b3557ec
chlangus <angusangus11@gmail.com>
2024-03-18 17:46:33 +0900

    Fix: Modal Container 만들어서 내용물 넣어줌

--- src/components/Layout/Modal.tsx
+++ src/components/Layout/Modal.tsx
@@ -2,24 +2,14 @@ import ReactDOM from 'react-dom';
 import { selectModal } from 'src/utils/selectModal';
 import { useAtomValue } from 'jotai';
 import { modalAtom } from 'src/store/store';
+import ModalContainer from '../Modal/ModalContainer';
 
 export default function Modal() {
   const modal = useAtomValue(modalAtom);
 
   return ReactDOM.createPortal(
     modal['status'] && (
-      <>
-        <div
-          role="button"
-          aria-label="Close Modal"
-          className="fixed top-0 bottom-0 w-screen h-screen bg-black opacity-70"
-        />
-        <div className="fixed top-0 flex items-center justify-center w-screen h-screen">
-          <div className="px-[2.8rem] pt-[3.2rem] pb-[2.8rem] bg-[#fff] rounded-[0.8rem]">
-            {selectModal(modal['name'])}
-          </div>
-        </div>
-      </>
+      <ModalContainer>{selectModal(modal['name'])}</ModalContainer>
     ),
     document.body
   );
