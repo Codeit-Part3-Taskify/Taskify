9b12fc03e24d5fd3381b5aa685b1e125830bc88c
haeyong9701 <haeyong9701@gmail.com>
2024-03-21 16:55:07 +0900

    Refactor: 대시보드 수정페이지 이름 변경

--- src/store/store.ts
+++ src/store/store.ts
@@ -3,12 +3,6 @@ import { atom } from 'jotai';
 import { ModalType } from 'src/types/modalTypes';
 import getDashboards from '../apis/getDashboards';
 
-// TODO 나중에 삭제
-export const dashboardsAtom = atomWithQuery(() => ({
-  queryKey: ['dashboards'],
-  queryFn: () => getDashboards()
-}));
-
 export const modalAtom = atom<{
   status: boolean;
   name: ModalType;
