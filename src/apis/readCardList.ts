642fd9a079360ae93d0d0878a3f76d7cce2ff9c9
Jnnsu <jspp120@gmail.com>
2024-03-21 15:15:50 +0900

    Remove: 불필요 토큰 코드 삭제

--- src/apis/readCardList.ts
+++ src/apis/readCardList.ts
@@ -1,14 +1,8 @@
 import axiosInstance from './axiosInstance';
 
-// 임시
-export const Authorization =
-  'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTI3NCwidGVhbUlkIjoiMy01IiwiaWF0IjoxNzEwNTA5OTgzLCJpc3MiOiJzcC10YXNraWZ5In0.bs7A-8tp8FT9PcL1uIvbSQEJJpKEesjHZc8z280ZtCk';
-
 // react-query과 관련이 없도록 사용
 const readCardList = async (columnId: number) => {
-  const { data } = await axiosInstance.get(`cards?columnId=${columnId}`, {
-    headers: { Authorization }
-  });
+  const { data } = await axiosInstance.get(`cards?columnId=${columnId}`);
   return data;
 };
 
