b4698e79f85b808feb8902d5c86c6e97b357693f
haeyong9701 <haeyong9701@gmail.com>
2024-03-22 13:02:41 +0900

    Feat: 대시보드 페이지네이션

--- src/components/Dashboard/DashboardList.tsx
+++ src/components/Dashboard/DashboardList.tsx
@@ -1,15 +1,30 @@
-import useDashboardList from 'src/hooks/useDashboardList';
 import { Dashboard } from 'src/types/dashboardTypes';
+import { usePagenationDashboardList } from 'src/hooks/usePagenationDashboardList';
 import DashboardItem from './DashboardItem';
+import PagenationButtons from '../Buttons/PagenationButtons';
 
 export default function DashboardList() {
-  const dashboards = useDashboardList();
+  const {
+    data,
+    allPage,
+    nowPage,
+    handleBackwardButtonClick,
+    handleForwardButtonClick
+  } = usePagenationDashboardList();
+
+  const dashboards = data?.dashboards ?? [];
 
   return (
     <div className="flex flex-col items-center gap-[0.3rem] pt-[2.2rem] tablet:mx-[1.2rem]">
       {dashboards.map((dashboard: Dashboard) => (
         <DashboardItem key={dashboard.id} dashboard={dashboard} />
       ))}
+      <PagenationButtons
+        allPage={allPage}
+        nowPage={nowPage}
+        handleBackwardButtonClick={handleBackwardButtonClick}
+        handleForwardButtonClick={handleForwardButtonClick}
+      />
     </div>
   );
 }
