236dceed953986e57291951eb277a9b3df97640a
Jnnsu <jspp120@gmail.com>
2024-03-22 15:32:31 +0900

    Style: 모달 버튼 텍스트 반응형 추가

--- src/components/Buttons/ModalResetButton.tsx
+++ src/components/Buttons/ModalResetButton.tsx
@@ -13,7 +13,7 @@ export default function ModalResetButton({ children }: ModalResetButtonProp) {
     <Button
       variant="secondary"
       type="reset"
-      customStyles="tablet:w-[12rem] tablet:h-[4.8rem] w-[13.8rem] h-[4.2rem] rounded-[0.8rem] text-[#787486] text-[1.6rem]"
+      customStyles="tablet:w-[12rem] tablet:h-[4.8rem] w-[13.8rem] h-[4.2rem] rounded-[0.8rem] text-[#787486] tablet:text-[1.6rem] text-[1.4rem]"
       onClick={() => setModal(prev => ({ ...prev, status: false }))}
     >
       {children}
