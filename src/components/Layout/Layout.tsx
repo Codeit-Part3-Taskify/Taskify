1fae552cdb48770c60567923d5d75c297eb0abcf
Jnnsu <jspp120@gmail.com>
2024-03-24 11:01:30 +0900

    Fix: Layout 간단하게 변경

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -7,13 +7,9 @@ export default function Layout() {
   return (
     <div className="relative flex">
       <Modal />
-      <div className="absolute top-0 z-0">
+      <Sidebar />
+      <div className="flex flex-col">
         <Navbar />
-      </div>
-      <div className="absolute top-0 left-0">
-        <Sidebar />
-      </div>
-      <div className="tablet:mt-[7rem] mt-[6rem] ml-[6.7rem] tablet:ml-[16rem] desktop:ml-[30rem]">
         <Outlet />
       </div>
     </div>
