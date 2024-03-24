883e5fb9b0668a4c0bdf8ce30182be195f49cd7c
Jnnsu <jspp120@gmail.com>
2024-03-25 06:30:50 +0900

    Style: 스크롤바 숨김

--- src/components/Dashboard/Column/Column.tsx
+++ src/components/Dashboard/Column/Column.tsx
@@ -11,7 +11,7 @@ export default function Column({ columnInfo }: Props) {
 
   return (
     <section
-      className="p-[1.2rem] border-r border-b  w-[calc(100vw-16rem)] tablet:p-[2rem] desktop:w-[35.4rem] shrink-0 h-screen overflow-y-scroll"
+      className="p-[1.2rem] border-r border-b  w-[calc(100vw-16rem)] tablet:p-[2rem] desktop:w-[35.4rem] shrink-0 h-screen overflow-y-scroll scrollbar-hide"
       ref={cardContainer}
       onScroll={handleScroll}
     >
