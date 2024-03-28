3d2605aa9cc21d5e65bad9f0ee9aa5ba0c139dd6
Jnnsu <jspp120@gmail.com>
2024-03-29 04:08:14 +0900

    Feat: 대시보드 페이지 swipe 기능 추가

--- src/pages/Dashboard/DashboardPage.tsx
+++ src/pages/Dashboard/DashboardPage.tsx
@@ -4,6 +4,9 @@ import Column from 'src/components/Dashboard/Column/Column';
 import AddColumn from 'src/components/Dashboard/Column/AddColumn';
 import { useQuery } from '@tanstack/react-query';
 import { ColumnData } from 'src/types/columnTypes';
+import { useRef } from 'react';
+import backwardArrow from 'src/assets/images/left-arrow.png';
+import forwardArrow from 'src/assets/images/right-arrow.png';
 
 export default function Dashboard() {
   const { boardId } = useParams();
@@ -11,13 +14,40 @@ export default function Dashboard() {
     queryKey: ['readColumnList', boardId],
     queryFn: () => readColumnList(boardId as string)
   });
+  const scrollContainerRef = useRef<HTMLDivElement>(null);
+
+  const scroll = (direction: any) => {
+    if (scrollContainerRef.current) {
+      const { current } = scrollContainerRef;
+      const scrollAmount = 500;
+      if (direction === 'left') {
+        current.scrollBy({ left: -scrollAmount, behavior: 'smooth' });
+      } else {
+        current.scrollBy({ left: scrollAmount, behavior: 'smooth' });
+      }
+    }
+  };
 
   return (
-    <main className="flex flex-col desktop:h-full desktop:flex-row">
-      {data?.data?.map((column: ColumnData) => (
-        <Column key={column.id} columnInfo={column} />
-      ))}
-      <AddColumn />
+    <main className="relative flex flex-col desktop:h-full desktop:flex-row">
+      <button
+        onClick={() => scroll('left')}
+        className="hover:bg-violet_8 w-[5rem] h-full bg-transparent absolute left-0"
+      >
+        <img src={backwardArrow} alt="왼쪽 화살표 아이콘" />
+      </button>
+      <div ref={scrollContainerRef} className="flex overflow-auto">
+        {data?.data?.map((column: ColumnData) => (
+          <Column key={column.id} columnInfo={column} />
+        ))}
+        <AddColumn />
+      </div>
+      <button
+        onClick={() => scroll('right')}
+        className="hover:bg-violet_8 w-[5rem] h-full bg-transparent absolute right-0"
+      >
+        <img src={forwardArrow} alt="오른쪽 화살표 아이콘" />
+      </button>
     </main>
   );
 }
