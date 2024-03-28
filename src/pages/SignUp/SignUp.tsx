38916776035dac0102dffbf220fee569ebd34f29
yulmai999 <37208116+yulmai999@users.noreply.github.com>
2024-03-28 15:47:49 +0900

    Feature: helmet 추가

--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -1,9 +1,13 @@
-import TaskifyLogo from '../../components/LogIn/TaskifyLogo';
+import { Helmet } from 'react-helmet-async';
 import SignUpForm from '../../components/LogIn/SignUpForm';
+import TaskifyLogo from '../../components/LogIn/TaskifyLogo';
 
 export default function LogIn() {
   return (
     <div className="flex flex-col justify-center items-center mt-[22.3rem]">
+      <Helmet>
+        <title>Taskify 회원가입</title>
+      </Helmet>
       <TaskifyLogo />
       <SignUpForm />
       <div className="text-[1.6rem]">
